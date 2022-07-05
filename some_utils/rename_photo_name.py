01f204912517736dc7102eda264f206416207357
HE_SE <a0987999103@gmail.com>
2022-07-05 11:40:26 +0800

    圖像檔案名稱轉換

--- some_utils/rename_photo_name.py
+++ some_utils/rename_photo_name.py
@@ -1,17 +1,23 @@
 import os
 
 
+# 照片檔案路徑
 data_path = '/Volumes/Pytorch/traffic_red_line'
 files_name = os.listdir(data_path)
+# 會先依據原始圖像名稱進行排序
 files_name.sort()
-cur_file = 0
-for file_name in files_name:
+total = 0
+
+for index, file_name in enumerate(files_name):
+    # 過濾一些不需要的檔案(Mac解壓縮時可能會夾帶一些不需要的檔案)
     if file_name[0] == '.':
         continue
     name = file_name.split('.')
     old_name = os.path.join(data_path, file_name)
-    new_name = os.path.join(data_path, str(cur_file) + '.' + name[1].lower())
+    # 這裡的名稱是依照當前index
+    new_name = os.path.join(data_path, str(index) + '.' + name[1].lower())
+    # 更新檔案名稱
     os.rename(old_name, new_name)
-    cur_file += 1
+    total += 1
 
-print(f"Total change {cur_file} files name.")
+print(f"Total change {total} files name.")
