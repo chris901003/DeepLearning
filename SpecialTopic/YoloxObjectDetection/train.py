01c4b115630c30f66675a135e435fcce6362f253
109project-deeplearning <109project.deeplearning@gmail.com>
2022-09-28 11:26:58 +0800

    develop on develop01

--- SpecialTopic/YoloxObjectDetection/train.py
+++ SpecialTopic/YoloxObjectDetection/train.py
@@ -10,6 +10,7 @@ from SpecialTopic.ST.net.lr_scheduler import get_lr_scheduler_yolox, set_optimiz
 import numpy as np
 from torch.backends import cudnn
 from utils_fit import fit_one_epoch
+# On develop01 writing
 
 
 def parse_args():
