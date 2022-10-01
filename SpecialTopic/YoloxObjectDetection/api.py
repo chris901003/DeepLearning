c477f24e97f01c020248865707a50ed2a3dd0f7b
chris901003 <a0987999103@gmail.com>
2022-10-01 09:15:25 +0800

    yolox即時偵測，添加構建模型api

--- SpecialTopic/YoloxObjectDetection/api.py
+++ SpecialTopic/YoloxObjectDetection/api.py
@@ -3,6 +3,7 @@ import numpy as np
 import PIL
 import torch
 from PIL import Image
+from SpecialTopic.ST.build import build_detector
 from utils import resize_image, cvtColor, preprocess_input, decode_outputs, non_max_suppression
 
 
@@ -30,3 +31,43 @@ def detect_image(model, device, image_info, input_shape, num_classes, confidence
     top_conf = results[0][:, 4] * results[0][:, 5].tolist()
     top_boxes = results[0][:, :4].tolist()
     return top_label, top_conf, top_boxes
+
+
+def init_model(cfg='auto', pretrained='none', num_classes=100, device='auto'):
+    if device == 'auto':
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    if cfg == 'auto':
+        cfg = {
+            'type': 'YoloBody',
+            'phi': 'l',
+            'backbone_cfg': {
+                'type': 'YOLOPAFPN'
+            },
+            'head_cfg': {
+                'type': 'YOLOXHead',
+                'num_classes': num_classes
+            }
+        }
+    model = build_detector(cfg)
+    if pretrained != 'none':
+        print(f'Load weights {pretrained}')
+        model_dict = model.state_dict()
+        pretrained_dict = torch.load(pretrained, map_location=device)
+        if 'model_weight' in pretrained_dict:
+            pretrained_dict = pretrained_dict['model_weight']
+        load_key, no_load_key, temp_dict = [], [], {}
+        for k, v in pretrained_dict.items():
+            if k in model_dict.keys() and np.shape(model_dict[k]) == np.shape(v):
+                temp_dict[k] = v
+                load_key.append(k)
+            else:
+                no_load_key.append(k)
+        model_dict.update(temp_dict)
+        model.load_state_dict(model_dict)
+        model = model.to(device)
+        print("\nSuccessful Load Key:", str(load_key)[:500], "……\nSuccessful Load Key Num:", len(load_key))
+        print("\nFail To Load Key:", str(no_load_key)[:500], "……\nFail To Load Key num:", len(no_load_key))
+        assert len(no_load_key) == 0, '給定的預訓練權重與模型不匹配'
+    else:
+        print('未加載預訓練權重，模型工作是幾乎無效的')
+    return model
