32ef937a104446452f84132aca01b729aa0649cf
chris901003 <a0987999103@gmail.com>
2022-10-02 22:15:18 +0800

    新增VIT模型，對於剩餘量進行分類

--- SpecialTopic/ST/net/layer.py
+++ SpecialTopic/ST/net/layer.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 from .basic import BaseConv, DWConv, ConvModule
-from ..build import build_activation
+from ..build import build_activation, build_norm
 
 
 class CSPLayer(nn.Module):
@@ -246,3 +246,112 @@ class BottleneckResnet(nn.Module):
         out += identity
         out = self.relu(out)
         return out
+
+
+class PatchEmbed(nn.Module):
+    def __init__(self, img_size=224, patch_size=16, in_channels=3, embed_dim=768, norm_layer=None):
+        super(PatchEmbed, self).__init__()
+        img_size = (img_size, img_size)
+        patch_size = (patch_size, patch_size)
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.grid_size = (img_size[0] // patch_size[0], img_size[1] // patch_size[1])
+        self.num_patches = self.grid_size[0] * self.grid_size[1]
+        self.proj = nn.Conv2d(in_channels, embed_dim, kernel_size=patch_size, stride=patch_size)
+        self.norm = norm_layer(embed_dim) if norm_layer else nn.Identity()
+
+    def forward(self, x):
+        batch_size, channel, height, width = x.shape
+        assert height == self.img_size[0] and width == self.img_size[1], \
+            f'輸入圖像大小與網路設定不符'
+        x = self.proj(x).flatten(2).transpose(1, 2)
+        x = self.norm(x)
+        return x
+
+
+class VitBlock(nn.Module):
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop_ratio=0., attn_drop_ratio=0.,
+                 drop_path_ratio=0., act_layer='Default', norm_layer='Default'):
+        super(VitBlock, self).__init__()
+        if act_layer == 'Default':
+            act_layer = dict(type='GELU')
+        if norm_layer == 'Default':
+            norm_layer = dict(type='LN')
+        self.norm1 = build_norm(norm_layer, dim)[1]
+        self.attn = Attention(dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale,
+                              attn_drop_ratio=attn_drop_ratio, proj_drop_ratio=drop_ratio)
+        self.drop_path = DropPath(drop_path_ratio) if drop_path_ratio > 0. else nn.Identity()
+        self.norm2 = build_norm(norm_layer, dim)[1]
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop_ratio)
+
+    def forward(self, x):
+        x = x + self.drop_path(self.attn(self.norm1(x)))
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop_ratio=0., proj_drop_ratio=0.):
+        super(Attention, self).__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = qk_scale or head_dim ** -0.5
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop_ratio)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop_ratio)
+
+    def forward(self, x):
+        batch_size, num_patch, channel = x.shape
+        qkv = self.qkv(x).reshape(batch_size, num_patch, 3, self.num_heads,
+                                  channel // self.num_heads).permute(2, 0, 3, 1, 4)
+        # [batch_size, num_heads, num_patches, embed_dim_per_head]
+        q, k, v = qkv[0], qkv[1], qkv[2]
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(batch_size, num_patch, channel)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+
+class Mlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer='Default', drop=0.):
+        super(Mlp, self).__init__()
+        if act_layer == 'Default':
+            act_layer = dict(type='GELU')
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = build_activation(act_layer)
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.drop(self.act(self.fc1(x)))
+        x = self.drop(self.fc2(x))
+        return x
+
+
+class DropPath(nn.Module):
+    # 目前只有在VIT上被使用到
+    def __init__(self, drop_prob=None):
+        super(DropPath, self).__init__()
+        self.drop_prob = drop_prob
+
+    @staticmethod
+    def drop_path(x, drop_prob: float = 0., training: bool = False):
+        if drop_prob == 0. or not training:
+            return x
+        keep_prob = 1 - drop_prob
+        shape = (x.shape[0],) + (1,) * (x.ndim - 1)
+        random_tensor = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device)
+        random_tensor.floor_()
+        output = x.div(keep_prob) * random_tensor
+        return output
+
+    def forward(self, x):
+        return self.drop_path(x, self.drop_prob, self.training)
