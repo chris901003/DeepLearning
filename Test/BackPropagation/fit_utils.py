5088df6b7d4e01ddfb5aab2ec8144bc64286ec2c
chris901003 <a0987999103@gmail.com>
2022-10-08 11:57:56 +0800

    應該是最後版本了，可以先訓練好權重，之後直接加載就可以

--- Test/BackPropagation/fit_utils.py
+++ Test/BackPropagation/fit_utils.py
@@ -15,13 +15,26 @@ def fit_one_epoch_without_batch(model, epoch, Epoch, train_dataloader, test_data
         labels[label[0]] = 1
         model.back_propagate(output, labels, lr)
         cnt += 1
-        pbar.set_postfix(**{'acc': acc / cnt,
-                            'lr': lr})
+        pbar.set_postfix(**{'acc': acc / cnt, 'lr': lr})
         if cnt == 1000:
             cnt = 0
             acc = 0
         pbar.update(1)
     pbar.clear()
+    pbar.close()
+
+    acc = 0
+    cnt = 0
+    pbar_val = tqdm(total=len(test_dataloader), desc=f'Epoch {epoch}/{Epoch}', postfix=dict, miniters=0.3)
+    for iteration, (image, label) in enumerate(test_dataloader):
+        output = model(image)[0]
+        ans = output.argmax()
+        acc += ans == label[0]
+        cnt += 1
+        pbar_val.set_postfix(**{'acc': acc / cnt, 'lr': lr})
+        pbar_val.update(1)
+    pbar_val.clear()
+    pbar_val.close()
 
 
 # with batch
@@ -33,16 +46,28 @@ def fit_one_epoch_with_batch(model, epoch, Epoch, train_dataloader, test_dataloa
         batch_size = image.shape[0]
         output = model(image).squeeze(axis=1)
         ans = output.argmax(axis=1)
-        acc += (ans == label).sum() / batch_size
+        acc += (ans == label).sum()
         # 構建one_hot編碼
         labels = np.eye(num_classes)[label]
         model.back_propagate(output, labels, lr)
-        cnt += 1
-        pbar.set_postfix(**{'acc': acc / cnt,
-                            'lr': lr})
+        cnt += batch_size
+        pbar.set_postfix(**{'acc': acc / cnt, 'lr': lr})
         if cnt == 1000:
             cnt = 0
             acc = 0
         pbar.update(1)
     pbar.clear()
+
+    acc = 0
+    cnt = 0
+    pbar_val = tqdm(total=len(train_dataloader), desc=f'Epoch {epoch}/{Epoch}', postfix=dict, miniters=0.3)
+    for iteration, (image, label) in enumerate(test_dataloader):
+        batch_size = image.shape[0]
+        output = model(image).squeeze(axis=1)
+        ans = output.argmax(axis=1)
+        acc += (ans == label).sum()
+        cnt += batch_size
+        pbar_val.set_postfix(**{'acc': acc / cnt, 'lr': lr})
+        pbar_val.update(1)
+    pbar_val.clear()
 
