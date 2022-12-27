299c6774ec25bcbc7ce41a6aea1e03e448f4368c
109project-deeplearning <109project.deeplearning@gmail.com>
2022-12-27 22:12:37 +0800

    構建Remain Eating Time Regression的onnx檔案

--- SpecialTopic/RemainEatingTime/RegressionModel/GenerateData/RecognizeWeightByYolo.py
+++ SpecialTopic/RemainEatingTime/RegressionModel/GenerateData/RecognizeWeightByYolo.py
@@ -12,12 +12,14 @@ def args_parser():
     parser.add_argument('--pretrained-path', type=str, default=r'C:\Checkpoint\YoloxWeightNumberDetection\first_'
                                                                r'version_1_8.pth')
     # 偵測影片路徑
-    parser.add_argument('--video-path', type=str, default=[r'C:\Dataset\vedio\Rice\1128_Dinner.mp4',
-                                                           r'C:\Dataset\vedio\Rice\1202_dinner_2.mp4',
-                                                           r'C:\Dataset\vedio\Rice\1204_dinner.mp4',
-                                                           r'C:\Dataset\vedio\Rice\1206_dinner_2.mov',
-                                                           r'C:\Dataset\vedio\Rice\1210_dinner.mp4'], nargs='+')
+    parser.add_argument('--video-path', type=str, default=[r'C:\Dataset\vedio\Rice\1217_rice.mp4',
+                                                           r'C:\Dataset\vedio\Rice\1220_donburi.mp4',
+                                                           r'C:\Dataset\vedio\Rice\1220_rice.mp4',
+                                                           r'C:\Dataset\vedio\Rice\1220_rice_2.mp4',
+                                                           r'C:\Dataset\vedio\Rice\1221_donburi_2.mp4',
+                                                           r'C:\Dataset\vedio\Rice\1221-donburi.mp4'], nargs='+')
     # 保存資料路徑
+    parser.add_argument('--save-path', type=str, default='remain')
     args = parser.parse_args()
     return args
 
